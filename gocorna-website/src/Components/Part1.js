e72032ff79d5c4b14a4812f90ccda7d4e69f29e2
HarshaMaria <harsha.maria@edstem.com>
2023-10-25 13:55:07 +0530

    Doing

--- gocorna-website/src/Components/Part1.js
+++ gocorna-website/src/Components/Part1.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Part1.css';
 import Icon from '../assets/Group 1.svg';
+import Group23 from '../assets/Group 23.svg';
 
 function Part1() {
   return (
@@ -20,10 +21,18 @@ function Part1() {
       </header>
       <div className="box">
         <div><h1>Take care of yours family's <span className="health">health.</span></h1></div>
-        <p>All in one destination for COVID-19 health queries. Consult 10,000+ health workers about your concerns.</p>
+        <p>All in one destination for COVID-19 health queries.
+     Consult 10,000+ health workers about your concerns.</p>
         <button className="get-started-button">
           GET STARTED
         </button>
+      </div>
+      <div className="icon-text-container">
+        <img src={Group23} alt="Group 23 icon" className="group-23-icon" />
+        <div>
+         <p className="text1">Stay safe with GoCorona</p>
+         <p className="text2">WATCH VIDEO</p>
+        </div>
       </div>
       {/* Rest of your component */}
     </div>
