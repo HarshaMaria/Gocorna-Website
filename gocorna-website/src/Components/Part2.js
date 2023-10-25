cfe6bfee46dc3f76b48bacfee05e9cb3f9c54225
HarshaMaria <harsha.maria@edstem.com>
2023-10-26 02:32:09 +0530

    Doing

--- gocorna-website/src/Components/Part2.js
+++ gocorna-website/src/Components/Part2.js
@@ -5,9 +5,14 @@ import Group22 from '../assets/Group22.svg';
 function Part2() {
   return (
     <div className="part2">
-    <img src={Group22}></img>
+    <div className="image1"><img src={Group22} alt="Group 22"></img></div>
+      <div className="content">
+        <h1>Stay safe with Go<span className="corona">Corona.</span></h1>
+        <p>24x7 Support and user friendly mobile platform to bring healthcare to your fingertips. Signup and be a part of the new health culture.</p>
+        <button>FEATURES</button> 
+      </div> 
     </div>
   );
 }
 
-export default Part2
+export default Part2;
